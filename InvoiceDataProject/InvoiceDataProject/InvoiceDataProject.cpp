537459e5cddad3d27393654ce1d055abc38a271b
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2024-09-24 19:04:18 -0700

    Initial repository

---
+++ InvoiceDataProject/InvoiceDataProject/InvoiceDataProject.cpp
@@ -0,0 +1,12 @@
+﻿// InvoiceDataProject.cpp : Defines the entry point for the application.
+//
+
+#include "InvoiceDataProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
